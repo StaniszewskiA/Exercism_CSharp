668884c046b22088ff822b68e85d976ab3263c9d
AStaniszewski <staniszewski01adam@gmail.com>
2024-06-01 19:03:36 +0200

    Ex 41

--- Ex41/PhoneNumberAnalysis.cs
+++ Ex41/PhoneNumberAnalysis.cs
@@ -1,14 +1,29 @@
 using System;
+using System.Text.RegularExpressions;
 
 public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        var phoneRegex = new Regex(@"^\d{3}-\d{3}-\d{4}$");
+
+        if(!phoneRegex.IsMatch(phoneNumber))
+        {
+            throw new ArgumentException("Invalid phone number format.");
+        }
+
+        var parts = phoneNumber.Split('-');
+        var areaCode = parts[0];
+        var prefix = parts[1];
+        var lineNumber = parts[2];
+
+        bool isNewYork = areaCode == "212";
+        bool isFake = prefix == "555";
+        string localNumber = lineNumber;
+
+        return (isNewYork, isFake, localNumber);
     }
 
-    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
-    {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
-    }
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) => phoneNumberInfo.IsFake;
+
 }
