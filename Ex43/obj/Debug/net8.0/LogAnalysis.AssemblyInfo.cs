463830c808bae742858adcd91308474219b58c01
AStaniszewski <staniszewski01adam@gmail.com>
2024-06-01 19:03:18 +0200

    Ex 41

--- Ex43/obj/Debug/net8.0/LogAnalysis.AssemblyInfo.cs
+++ Ex43/obj/Debug/net8.0/LogAnalysis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogAnalysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d9fb00bab42e8b943a8cfb30794c8291723d66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6a344f0c8e21e994977ff4d9b885a70a1423b76")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogAnalysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogAnalysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
