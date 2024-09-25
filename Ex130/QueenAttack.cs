c48f4a3f0b7d006067826b28cdd9401084c4a7c3
AStaniszewski <staniszewski01adam@gmail.com>
2024-09-25 16:01:03 +0200

    25.09 session

--- Ex130/QueenAttack.cs
+++ Ex130/QueenAttack.cs
@@ -16,14 +16,14 @@ public static class QueenAttack
 {
     public static bool CanAttack(Queen white, Queen black) =>
         white.Row == black.Row ||
-        white.Col = black.Col ||
-        Math.Ab(white.Row - black.Row) == Math.Abs(black.Col - white.Col);
+        white.Column == black.Column ||
+        Math.Abs(white.Row - black.Row) == Math.Abs(black.Column - white.Column);
 
     public static Queen Create(int row, int column)
     {
         if (row is < 0 or > 7) throw new ArgumentOutOfRangeException(nameof(row));
         if (column is < 0 or > 7) throw new ArgumentOutOfRangeException(nameof(column));
 
-        return Queen(row, column);
+        return new Queen(row, column);
     }
 }
